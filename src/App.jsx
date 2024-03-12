f56334ab93a392369533ea3c65b53c01e3abd6fe
Alvin Wang <chengji3728@gmail.com>
2024-03-12 11:14:38 +0800

    feat: add policy ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import Header from "./components/Header.jsx";
 import Banner from "./components/Banner.jsx";
 import Introduce from "./components/Introduce.jsx";
 import Campaign from "./components/Campaign.jsx";
+import Policy from "./components/Policy.jsx";
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <Banner />
       <Introduce />
       <Campaign />
-    </div>
+      <Policy />
+    </>
   );
 }
 
